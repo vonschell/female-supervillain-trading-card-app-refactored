2912ee13858f3a8e2b615e6744cf522ea9f9b2e7
Vonschell <vonschell02@gmail.com>
2025-03-10 18:41:57 +0900

    Create database structure for villain characteristics

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 # Initialize the Flask application
 app = Flask(__name__)
@@ -8,8 +9,19 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///villain.db"
 
 db = SQLAlchemy(app)
 
+class Villain(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String(80), unique=True, nullable=False)
+  description = db.Column(db.String(250), nullable=False)
+  interests = db.Column(db.String(250), nullable=False)
+  url = db.Column(db.String(250), nullable=False)
+  date_added = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+def __repr__(self):
+  return "<Villain "+ self.name + ">"
+
 @app.route("/")
-def hello_world():
+def villains_cards():
   return render_template("villain.html")
 
 # Run the flask server
