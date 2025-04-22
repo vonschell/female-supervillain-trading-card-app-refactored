127c7f9318dbbd67bf0af977926200145cd2b654
vonschell <vonschell02@gmail.com>
2025-04-22 16:46:37 +0200

    Changing the discription column's length from 250 to 120

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ db = SQLAlchemy(app)
 class Villain(db.Model):
   id = db.Column(db.Integer, primary_key=True)
   name = db.Column(db.String(80), unique=True, nullable=False)
-  description = db.Column(db.String(250), nullable=False)
+  description = db.Column(db.String(120), nullable=False)
   interests = db.Column(db.String(250), nullable=False)
   url = db.Column(db.String(250), nullable=False)
   date_added = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
