4f02f5afa97467f3d9998138402af8150ffc73f0
vonschell <vonschell02@gmail.com>
2025-05-16 10:04:23 +0200

    Import func from SQLAlchemy to enable case-insensitive query

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import func 
 from datetime import datetime
 
 # Initialize the Flask application
